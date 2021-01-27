021ac292a8ec6daac3aebfe64979e350d2161b14
Ashton-Stom <ashtonrstom@gmail.com>
2021-01-26 19:44:27 -0800

    Calculator

--- review/src/App.js
+++ review/src/App.js
@@ -2,14 +2,41 @@ import React, { useReducer } from "react";
 import reducer from './reducers/calcReducer';
 import "./styles.css";
 
+const initialState = {
+  currentValue: 0,
+  memory: 0
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleAddTwo = e => {
+    e.preventDefault();
+    dispatch({ type: 'ADD', payload: 2 })
+  }
+
+  const handleSubTwo = e => {
+    e.preventDefault();
+    dispatch({ type: 'SUBTRACT', payload: 2 })
+  }
+
+  const handleClear = e => {
+    e.preventDefault();
+    dispatch({ type: 'CLEAR', payload: 0 })
+  }
+
   return (
     <div className="App">
-      <textarea rows="1" value="0" id="total" type="text" name="ans"></textarea>
+      <textarea rows="1" value={state.currentValue} id="total" type="text" name="ans"></textarea>
       <br />
-      <button type="button" className="btn">
-        +
+      <button type="button" className="btn" onClick={handleAddTwo}> 
+        +2
+      </button>
+      <button type="button" className="btn" onClick={handleSubTwo}> 
+        -2
+      </button>
+      <button type="button" className="btn" onClick={handleClear}> 
+        CLEAR
       </button>
     </div>
   );
